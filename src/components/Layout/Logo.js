283e8f3d9815ff1ecf40c677d1ed343d865ea62c
Taras.Kravets <taraskravets95@gmail.com>
2023-02-28 23:20:20 +0200

    feat (Header): added Header component

---
+++ src/components/Layout/Logo.js
@@ -0,0 +1,6 @@
+import styles from '@/styles/components/Layout/Logo.module.css';
+
+function Logo() {
+	return <div className={styles.logo}>Taraskiiin Next Blog</div>;
+}
+export default Logo;
---
+++ src/components/Layout/MainNavigation.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import Logo from '@/components/Layout/Logo';
+
+import styles from '@/styles/components/Layout/MainNavigation.module.css';
+
+const headerLinks = [
+	{ link: '/posts', title: 'Posts' },
+	{ link: '/contacts', title: 'Contacts' },
+];
+
+function MainNavigation() {
+	return (
+		<header className={styles.header}>
+			<Link href='/'>
+				<Logo />
+			</Link>
+			<nav>
+				<ul>
+					{headerLinks.map((headerLink) => (
+						<li key={headerLink.title}>
+							<Link href={headerLink.link}>{headerLink.title}</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	);
+}
+
+export default MainNavigation;
