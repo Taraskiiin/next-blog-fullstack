bab266b8ec873f66a20182bf27002a2d13884359
Taras.Kravets <taraskravets95@gmail.com>
2023-03-01 01:18:28 +0200

    feat(allPost): added getAllPosts util

--- utils/PostsUtil.js
+++ utils/PostsUtil.js
@@ -8,10 +8,24 @@ function getPostData(fileName) {
 	const filePath = path.join(postsPath, fileName);
 	const fileContent = fs.readFileSync(filePath, 'utf-8');
 	const { data, content } = matter(fileContent);
+	const postSlug = fileName.replace(/\.md$/, '');
+	const postData = {
+		slug: postSlug,
+		...data,
+		content,
+	};
 
-	const postData = {};
+	return postData;
 }
 
 function getAllPosts() {
-	const postsFiles = fs.readFileSync(postsPath);
+	const postFiles = fs.readFileSync(postsPath);
+	const allPosts = postFiles.map((postFile) => {
+		return getPostData(postFile);
+	});
+
+	const sortedPosts = allPosts.sort((postA, postB) =>
+		postA.date > postB.date ? -1 : 1
+	);
+	return sortedPosts;
 }
