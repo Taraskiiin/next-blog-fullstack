5f5cc497077b8925d6cedc30b8916509191fa423
Taras.Kravets <taraskravets95@gmail.com>
2023-03-01 17:09:19 +0200

    feat(customRenderers): added custom render to ReactMarkdown

--- src/components/Posts/PostDetail/PostContent.js
+++ src/components/Posts/PostDetail/PostContent.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import PostHeader from '@/components/Posts/PostDetail/PostHeader';
 
@@ -5,11 +6,44 @@ import styles from '@/styles/components/Posts/PostDetail/PostContent.module.css'
 
 function PostContent({ post }) {
 	const imagePath = `/assets/projects/${post.image}`;
-	console.log(imagePath);
+
+	const customRenderers = {
+		paragraph(paragraph) {
+			const { node } = paragraph;
+
+			if (node.children[0].type === 'image') {
+				const image = node.children[0];
+
+				return (
+					<div className={styles.image}>
+						<Image
+							src={`/assets/${image.url}`}
+							alt={image.alt}
+							width={250}
+							height={250}
+						/>
+					</div>
+				);
+			}
+
+			return <p>{paragraph.children}</p>;
+		},
+
+		code(code) {
+			const { language, value } = code;
+			return (
+				<SyntaxHighlighter
+					style={atomDark}
+					language={language}
+					children={value}
+				/>
+			);
+		},
+	};
 	return (
 		<article className={styles.content}>
 			<PostHeader title={post.title} image={imagePath} />
-			<ReactMarkdown>{post.content}</ReactMarkdown>
+			<ReactMarkdown renderers={customRenderers} children={post.content} />
 		</article>
 	);
 }
